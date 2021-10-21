a8582e7e1552e7d4626971342fa6e721387741ff
FelipeUTFPR <fps2887@gmail.com>
2021-10-20 22:25:21 -0300

    correção bugs

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,14 +4,28 @@ describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
             expect(gerarNumeroAleatorio(20, -5))
-                .toBe(-0);
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('início maior ou igual ao fim', () => {
+            expect(gerarNumeroAleatorio(20, 10))
+                .toBe(-1);
         });
     });
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
-                .toBe('comprimento invalid');
+                .toBe('comprimento invalido');
+        });
+    }); 
+
+    describe('acharCaracter', () => {
+        test('comprimento valido', () => {
+            expect(acharCaracter(5, 'abcde', 'c'))
+                .toBe('caracter encontrado');
         });
     });    
 });
